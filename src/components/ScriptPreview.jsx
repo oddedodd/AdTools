0b857684b07957e7fad7bd2b58c65881915aa4d6
Odd Egil Hegge Selnes <post@odde.org>
2025-03-23 12:47:12 +0100

    starting with useEffect

--- src/components/ScriptPreview.jsx
+++ src/components/ScriptPreview.jsx
@@ -1,14 +1,20 @@
-function ScriptPreview( { scriptContent } ) {
-    console.log(scriptContent);
+import { useEffect } from "react";
 
-    return (
-       <>
-       <div className="mt-4 p-4 border border-gray-300 rounded bg-gray-100">
+function ScriptPreview({ scriptContent }) {
+  console.log(scriptContent);
+
+  useEffect(() => {
+    console.log(scriptContent);
+  }, []);
+
+  return (
+    <>
+      <div className="mt-4 p-4 border border-gray-300 rounded bg-gray-100">
         <h2 className="text-lg font-bold mb-2">Innhold i script-tag</h2>
-       </div>
-       <div dangerouslySetInnerHTML={{ __html: scriptContent }} />
-       </>
-    );
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: scriptContent }} />
+    </>
+  );
 }
 
 export default ScriptPreview;
